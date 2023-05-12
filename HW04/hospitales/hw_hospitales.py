22ec50db1d16505fdaa8a2065179022d9a14f0ca
knowai <dev.know.ai@gmail.com>
2023-05-11 23:13:26 -0400

    [UPD] comments in english

--- HW04/hospitales/hw_hospitales.py
+++ HW04/hospitales/hw_hospitales.py
@@ -1,22 +1,32 @@
+r'''A partir del csv hospitales2.csv, generar un archivo CSV de salida, 
+que contenga los siguientes campos en este orden:
+* latitude
+* longitude
+* name
+* label
+Con los correspondientes datos extraídos del CSV original, el campo name
+tiene que corresponder con la dirección del hospital, y el campo label con
+el nombre del hospital.'''
 import pandas as pd
 
+# Loading .csv file into DataFrame
 df = pd.read_csv('./hospitales2.csv')
-
+# New Columns initialization
 latitude = list()
 longitude = list()
-
+# Loop for transform column where are coordinates
 for index, row in df.iterrows():
 
-    _latitude, _longitude = row['WKT'].replace("POINT ", "").replace("(", "").replace(")", "").split(" ")
+    _latitude, _longitude = row['WKT'].replace("POINT (", "").replace(")", "").split(" ")
     latitude.append(float(_latitude))
     longitude.append(float(_longitude))
 
-# Creando nuevas columnas
+# Inserting Latitude and Longitud column
 df['latitude'] = latitude
 df['longitude'] = longitude
-# Renombrando columnas
+# Renaming Columns
 df.rename(columns={"DOM_NORMA": "name", "NOMBRE": "label"}, inplace=True)
-# Reordenar las columnas
+# Reordering Columns
 df = df[['latitude', 'longitude', 'name', 'label']]
 
 print(df.head())
