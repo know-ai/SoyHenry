f4c24c5067247df2a1202efac42d7677af790dbe
knowai <dev.know.ai@gmail.com>
2023-05-11 23:20:52 -0400

    [FIX] Changing latitude and longitude position

--- HW04/hospitales/hw_hospitales.py
+++ HW04/hospitales/hw_hospitales.py
@@ -6,7 +6,10 @@ que contenga los siguientes campos en este orden:
 * label
 Con los correspondientes datos extraídos del CSV original, el campo name
 tiene que corresponder con la dirección del hospital, y el campo label con
-el nombre del hospital.'''
+el nombre del hospital.
+
+Ejemplo de visualización en: https://www.gpsvisualizer.com
+'''
 import pandas as pd
 
 # Loading .csv file into DataFrame
@@ -17,7 +20,7 @@ longitude = list()
 # Loop for transform column where are coordinates
 for index, row in df.iterrows():
 
-    _latitude, _longitude = row['WKT'].replace("POINT (", "").replace(")", "").split(" ")
+    _longitude, _latitude = row['WKT'].replace("POINT (", "").replace(")", "").split(" ")
     latitude.append(float(_latitude))
     longitude.append(float(_longitude))
 
@@ -29,4 +32,4 @@ df.rename(columns={"DOM_NORMA": "name", "NOMBRE": "label"}, inplace=True)
 # Reordering Columns
 df = df[['latitude', 'longitude', 'name', 'label']]
 
-print(df.head())
+df.to_csv("./hospitales_cleaned.csv")
